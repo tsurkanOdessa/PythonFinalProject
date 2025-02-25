564df217adc5cd776ab89e94b9da441efd7640b0
tsurkanOdessa <set.tsurkan@gmail.com>
2025-02-25 16:35:15 +0100

    add app.py and func run_console_app():

---
+++ app.py
@@ -0,0 +1,83 @@
+from models.Searcher import Searcher
+
+
+def run_web_app():
+    return None
+
+def run_console_app():
+    while True:
+        print("Выберите вариант поиска:")
+        print("1. Поиск по жанру")
+        print("2. Поиск по Наименованию")
+        print("3. ТОП запросов поиска фильмов по популярности")
+        print("4. ТОП запросов поиска фильмов по дате")
+        print("B. Выход (Back) - для возврата в предыдущее меню")
+
+        search_choice = input("Введите ваш выбор: ").strip().upper()
+        searcher_in_db = Searcher()
+        if search_choice == '1':
+            genres = searcher_in_db.get_genres()
+            print("Доступные жанры:")
+            for index, genre in enumerate(genres, start=1):
+                print(f"{index}. {genre}")
+
+            genre_choice = input("Введите номер жанра: ").strip()
+            try:
+                genre_index = int(genre_choice) - 1
+                if 0 <= genre_index < len(genres):
+                    selected_genre = genres[genre_index][0]
+                    films = searcher_in_db.get_films(None, selected_genre, None)
+                    print_films(films)
+                else:
+                    print("Неверный выбор жанра.")
+            except ValueError:
+                print("Пожалуйста, введите корректный номер.")
+
+        elif search_choice == '2':
+            film_name = input("Введите наименование фильма: ").strip()
+            films = searcher_in_db.get_films(film_name, None, None)
+            print_films(films)
+
+        elif search_choice == '3':
+            break
+        elif search_choice == '4':
+            break
+        elif search_choice == 'B':
+            break
+
+def print_films(films):
+    count = 0
+    for film in films:
+        print(f"{count + 1}. {film[0]} - ({film[1]})")
+        count += 1
+
+        if count % 10 == 0 and count < len(films):
+            choice = input("Нажмите N (Next) чтобы показать следующие 10 фильмов или Q (Quit) для выхода: ").strip().lower()
+            if choice == 'q':
+                break
+
+
+
+
+def main():
+    while True:
+        print("Выберите вариант запуска приложения:")
+        print("1. Веб-режим")
+        print("2. Консольный режим")
+        print("Q. Выход")
+
+        choice = input("Сделайте выбор (1, 2 или Q для выхода): ").strip().upper()
+
+        if choice == '1':
+            run_web_app()
+        elif choice == '2':
+            run_console_app()
+        elif choice == 'Q':
+            print("Выход из программы.")
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, введите 1, 2 или Q.")
+            main()
+
+if __name__ == '__main__':
+    main()
