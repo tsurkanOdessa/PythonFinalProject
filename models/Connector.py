928e9affad9bf1216b189584ae2e3b2c3cab0449
tsurkanOdessa <set.tsurkan@gmail.com>
2025-02-25 16:23:41 +0100

    add file Connector.py

---
+++ models/Connector.py
@@ -0,0 +1,41 @@
+import configparser
+
+import mysql.connector as connector
+from mysql.connector import Error
+
+
+class Connector:
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
+
+    def get_db_connection(self,settings_name):
+        try:
+            config = configparser.ConfigParser()
+            config.read('settings.ini')
+        except configparser.Error as e:
+            print(f"Ошибка! Отсутствует, либо пустой файл settings.ini: {e}")
+            return None
+
+        host = config[settings_name]['host']
+        user = config[settings_name]['user']
+        password = config[settings_name]['password']
+        database = config[settings_name]['database']
+        charset = 'utf8mb4'
+
+        try:
+            conn = connector.connect(
+                host=host, user=user, password=password, database=database, charset=charset
+            )
+            if conn.is_connected():
+                self.connection = conn
+                self.cursor = conn.cursor()
+                return self.connection, self.cursor
+        except Error as e:
+            print(f"Ошибка: {e}")
+            return None
+
+    def close_connect(self):
+        if self.connection.is_connected():
+            self.cursor.close()
+            self.connection.close()
